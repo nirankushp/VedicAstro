0a3adbc825f8aaf97c52c97f7279849d96fa88d5
nirankushp <nirankush@gmail.com>
2025-05-23 16:23:18 -0700

    Update utils.py

--- vedicastro/utils.py
+++ vedicastro/utils.py
@@ -115,7 +115,7 @@ def get_utc_offset(timezone_loc: str, date: datetime):
     return utc_offset_str, utc_offset
 
 def calculate_pada(sidereal_degree: float):
-    \"\"\"Calculates the pada (1–4) based on sidereal degree, anchored to the start of each nakshatra.\"\"\"
+   """Calculates the pada (1–4) based on sidereal degree, anchored to the start of each nakshatra."""
     nakshatra_deg = 13 + 1/3  # 13.3333°
     nakshatra_index = int(sidereal_degree / nakshatra_deg)
     nakshatra_start = nakshatra_index * nakshatra_deg
